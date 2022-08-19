c8993a878d802effc0d252b08807bf233159e0f0
Mohammad <mohammadimaan484@gmail.com>
2022-08-19 05:27:05 +0300

    -

---
+++ script.js
@@ -0,0 +1,25 @@
+inputs = document.querySelectorAll('input')
+inputs.forEach((input)=>{
+  input.setAttribute('isrequired', 'false')
+})
+
+pwd = document.getElementById('pwd');
+pwd_confirm = document.getElementById('pwd-confirm');
+
+const password_match_function = () => {
+  if(pwd.value == pwd_confirm.value) {
+    pwd.setCustomValidity('');
+    pwd_confirm.setCustomValidity('');
+    document.querySelector('.pwd-mismatch-error').style.display = 'none';
+  }
+  else {
+    pwd.setCustomValidity('Password doesnt match');
+    pwd_confirm.setCustomValidity('Password doesnt match');
+    document.querySelector('.pwd-mismatch-error').style.display = 'block';
+  }
+}
+
+pwd.addEventListener('input', password_match_function);
+pwd_confirm.addEventListener('input', password_match_function);
+
+
